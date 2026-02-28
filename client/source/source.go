57ff9174ccbaa92f31a2a98c0a2075a5e4312f31
Chance Seger <chance.seger@gmail.com>
2026-02-28 14:50:27 -0600

    example showing making an http request

--- client/source/source.go
+++ client/source/source.go
@@ -1,13 +1,28 @@
 // This file contains the source the client will send to the server and then be executed by the server.
 package source
 
-import "log"
+import (
+	"io"
+	"log"
+	"net/http"
+)
 
 func Function() {
-	log.Println("This is working")
-	function()
-}
-
-func function() {
-	log.Println("Inside little function")
+	log.Println("--- Making http request ---")
+	r, err := http.NewRequest(http.MethodGet, "https://www.google.com", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Request created:", r.URL)
+	resp, err := http.DefaultClient.Do(r)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	log.Println("Response received:", resp.Status)
+	b, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Response body length (bytes):", len(b))
 }
